714f1a499dc350a9e0716bf77929b196e15b548d
Afaz Deen <AfazD@99x.lk>
2019-11-21 10:51:20 +0530

    Final version

---
+++ LogAnalyzer/main.cpp
@@ -0,0 +1,15 @@
+#include <map>
+#include <string>
+#include <iostream>
+#include "CPlusPlusEntry.h"
+//#include "D:/vcpkg/vcpkg/installed/x86-windows/include/json/version.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+        CPlusPlusEntry cppEntry;
+        cppEntry.RunDefault();
+
+    return 0;
+}
